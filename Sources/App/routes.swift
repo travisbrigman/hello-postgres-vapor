70013a7d4045ffe0730c5ab7498ebc15d2eb7598
travisbrigman <travis1000@gmail.com>
2020-12-12 05:40:27 -0600

    added put and delete methods

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -13,6 +13,27 @@ func routes(_ app: Application) throws {
             .unwrap(or: Abort(.notFound))
     }
     
+    app.put("movies") { req -> EventLoopFuture<HTTPStatus> in
+        let movie = try req.content.decode(Movie.self)
+        
+        return Movie.find(movie.id, on:  req.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap {
+                $0.title = movie.title
+                return $0.update(on: req.db).transform(to: .ok)
+        }
+        
+    }
+    
+    app.delete("movies",":movieid") { req -> EventLoopFuture<HTTPStatus> in
+        Movie.find(req.parameters.get("movieid"), on: req.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap {
+                $0.delete(on: req.db)
+        }.transform(to: .ok)
+        
+    }
+    
     app.post("movies"){ req -> EventLoopFuture<Movie> in
       let movie = try req.content.decode(Movie.self) //content = body of httprequest
         return movie.create(on: req.db).map {movie}
