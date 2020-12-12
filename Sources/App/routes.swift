9471727b6be079c999a94204365a81eb98b806fc
travisbrigman <travis1000@gmail.com>
2020-12-11 21:44:03 -0600

    adding routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,14 +2,21 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req in
-        return req.view.render("index", ["title": "Hello Vapor!"])
+    app.get("movies"){ req in
+        Movie.query(on: req.db).all()
     }
-    app.get { req in
-        return "It works!"
+    
+    //movies/id
+    
+    app.get("movies", ":movieid") { req -> EventLoopFuture<Movie> in
+        Movie.find(req.parameters.get("movieid"), on: req.db)
+            .unwrap(or: Abort(.notFound))
+    }
+    
+    app.post("movies"){ req -> EventLoopFuture<Movie> in
+      let movie = try req.content.decode(Movie.self) //content = body of httprequest
+        return movie.create(on: req.db).map {movie}
+        
     }
 
-    app.get("hello") { req -> String in
-        return "Hello, world!"
-    }
 }
